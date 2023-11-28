3e676287036fe004ad834561d81b4e999cdd1f0f
cobocho <rlaalsrb1111@naver.com>
2023-11-28 14:52:02 +0900

    feat: `BridgeGame` 기능 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,20 +1,58 @@
+const BridgeMaker = require('./BridgeMaker.js');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator.js');
+const Bridge = require('./domain/Bridge/Bridge.js');
+const { HardDeck, WeakDeck } = require('./domain/Deck/index.js');
+const Stack = require('./domain/Stack/Stack.js');
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+
+  #try = 0;
+
+  constructor(size) {
+    const stacks = Array.from(BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator), (lane) => {
+      if (lane === Bridge.LANE_NAMES.up) {
+        return [Stack.of([HardDeck.of(), WeakDeck.of()])];
+      }
+      return [Stack.of([WeakDeck.of(), HardDeck.of()])];
+    });
+    this.#bridge = Bridge.of(stacks);
+  }
+
+  static of(size) {
+    return new BridgeGame(size);
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   * @param {string} lane
+   * @returns {import('./domain/Bridge/Bridge.js').BridgeResult}
    */
-  move() {}
+  move(lane) {
+    return this.#bridge.cross(lane);
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
    * <p>
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  retry() {}
+  retry() {
+    this.#try += 1;
+    this.#bridge.init();
+  }
+
+  isCompleted() {
+    return {
+      result: this.#bridge.isCompleted(),
+      try: this.#try,
+    };
+  }
 }
 
 module.exports = BridgeGame;
